1ab91776b3b58de3b04c556dd66138f633d09dd3
ConnorBieszk <84150127+pyluaVR@users.noreply.github.com>
2023-10-29 19:58:40 -0500

    Added all the mod files

---
+++ Plugin.cs
@@ -0,0 +1,48 @@
+﻿using BepInEx;
+using System;
+using System.IO;
+using UnityEngine;
+using Utilla;
+
+namespace llgtag_backend
+{
+    [ModdedGamemode]
+    [BepInDependency("org.legoandmars.gorillatag.utilla", "1.6.10")]
+    [BepInDependency("com.mrbanana.gorillatag.multigorillainteraction")]
+    [ModdedGamemode("llgtag", "LEGACIES", Utilla.Models.BaseGamemode.Infection)]
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static void customLogger(string logMessage, TextWriter w)
+        {
+            w.Write("\r\nLog Entry : ");
+            w.WriteLine($"{DateTime.Now.ToLongTimeString()} {DateTime.Now.ToLongDateString()}");
+            w.WriteLine("  :");
+            w.WriteLine($"  :{logMessage}");
+            w.WriteLine("-------------------------------");
+        }
+
+        public static void customLoggerDump(StreamReader r)
+        {
+            string line;
+            while ((line = r.ReadLine()) != null)
+            {
+                Console.WriteLine(line);
+            }
+        }
+
+        void Start()
+        {
+
+            Utilla.Events.GameInitialized += OnGameInitialized;
+        }
+
+        void OnGameInitialized(object sender, EventArgs e)
+        {
+            using (StreamWriter w = File.AppendText("lorienlegaciesmod.log"))
+            {
+                customLogger("Lorien Legacies Gamemode Initalized", w);
+            }
+        }
+    }
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace llgtag_backend
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.pyluadotcode.gorillatag.llgtag-backend";
+        public const string Name = "llgtag_backend";
+        public const string Version = "1.0.0";
+    }
+}
